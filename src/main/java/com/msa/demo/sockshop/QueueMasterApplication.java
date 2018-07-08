b7207d65e9db077747dd891b248797e182c1c1cc
Guoqing <zhaogq10@163.com>
2018-07-09 00:44:24 +0800

    Queue master: listen to rabbit mq

---
+++ src/main/java/com/msa/demo/sockshop/QueueMasterApplication.java
@@ -0,0 +1,13 @@
+package com.msa.demo.sockshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.servicecomb.springboot.starter.provider.EnableServiceComb;
+
+@SpringBootApplication
+@EnableServiceComb
+public class QueueMasterApplication {
+    public static void main(String[] args) throws InterruptedException {
+        SpringApplication.run(QueueMasterApplication.class, args);
+    }
+}
